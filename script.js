75ed8beb9c2a78e6a9cfda64246e7580951f3345
vector2405 <vectormobius@gmail.com>
2026-09-08 20:53:02 +0200

    portfolio hamburger

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const sections = document.querySelectorAll("section");
 const navLinks = document.querySelectorAll(".nav-link");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", function(){
+    navMenu.classList.toggle("active");
+});
+
 window.addEventListener("scroll", function (){
     sections.forEach(function(section){
         const sectionTop = section.offsetTop - 100;
